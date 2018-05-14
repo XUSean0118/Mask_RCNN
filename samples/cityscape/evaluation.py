b7bd59d9326de9cb3ebac4f23ebeae33d11422c7
XUSean0118 <susean0118@gmail.com>
2018-05-14 15:05:20 +0800

    add measure methods

--- samples/cityscape/evaluation.py
+++ samples/cityscape/evaluation.py
@@ -2,6 +2,7 @@ import os
 import sys
 import time
 import argparse
+import cv2
 import numpy as np
 
 # Root directory of the project
@@ -32,6 +33,9 @@ def get_arguments():
                         help="Snippets length.")
     parser.add_argument("--fix", action="store_true",
                         help="Fix key frame.")
+    parser.add_argument("--method", type=int, default=2,
+                        choices=[0, 1, 2],
+                        help="0 = frame_difference, 1 = flow_magnitude, 2 = confidence_score")
     parser.add_argument("--target", type=float, default=85,
                         help="confidence score threshold.")
     return parser.parse_args()
@@ -61,24 +65,28 @@ def main():
     warp = Warp(config=config)
     decision = Decision(config=config)
 
-    model_path = ""
+    model_path = "/home/susean/Mask_RCNN/logs/after50S/mask_rcnn_cityscapes_0060.h5"
     resnet.load_weights(model_path, by_name=True)
     flownet.load_weights(model_path, by_name=True)
     maskrcnn.load_weights(model_path, by_name=True)
-    decision.load_weights("", by_name=True)
+    decision.load_weights("/home/susean/Mask_RCNN/logs/after50S/deciosion_0182_3.02.h5", by_name=True)
 
     AP50s = []
     APs = []
     seg_step = 0
     flow_step = 0
     score = 0
+    if args.method == 2:
+        target = -args.target
+    else:
+        target = args.target
     for image_id in range(len(dataset.image_ids)):
          # Load image and ground truth data
         image, image_meta, gt_class_id, gt_bbox, gt_mask =\
             modellib.load_image_gt(dataset, config,
                                    image_id, use_mini_mask=False)
 
-        current = np.expand_dims(image[:,:,3:], 0)
+        current = np.expand_dims(image[:,:,3:], 0)# when args.fix it is key frame
         image_metas = np.expand_dims(image_meta, 0)
 
         if args.fix:
@@ -93,9 +101,21 @@ def main():
             if image_id % args.num_frames != 0:
                 images = np.concatenate([current, key], 3)
                 flow, flow_feature = flownet.keras_model.predict(images)
-                score = decision.keras_model.predict(flow_feature)
-
-            if score < args.target or image_id % args.num_frames == 0:
+                if args.method == 0:
+                    im1_gray = cv2.cvtColor(np.squeeze(current), cv2.COLOR_RGB2GRAY)
+                    im2_gray = cv2.cvtColor(np.squeeze(key), cv2.COLOR_RGB2GRAY)
+                    score = np.mean(np.abs(im1_gray - im2_gray))
+                    #print("step: {:4d} frame difference: {:.3f}".format(image_id, score))
+                else:
+                    if args.method == 1:
+                        flow_mag = flow*4
+                        score = np.mean(np.sqrt(np.square(flow_mag[:,:,:,0]) + np.square(flow_mag[:,:,:,1])))
+                        #print("step: {:4d} flow magnitude: {:.3f}".format(image_id, score))
+                    elif args.method == 2:
+                        score = -decision.keras_model.predict(flow_feature)[0][0]
+                        #print("step: {:4d} predict score: {:.3f}".format(image_id, -score))
+                       
+            if score > target or image_id % args.num_frames == 0:
                 seg_step += 1
                 key_P2, key_P3, key_P4, key_P5, key_P6 = resnet.keras_model.predict(current)
                 key = current
@@ -119,9 +139,9 @@ def main():
             AP = utils.compute_ap_range(gt_bbox, gt_class_id, gt_mask,
                            result["rois"], result["class_ids"], result["scores"], result['masks'], verbose=0)
             APs.append(AP)
-            print("step: {:3d}, AP50: {:.3f}, mAP: {:.3f}".format(image_id, np.mean(AP50s), np.mean(APs)))
+            print("step: {:4d}, AP50: {:.3f}, mAP: {:.3f}".format(image_id, np.mean(AP50s), np.mean(APs)))
 
-    print("step: {:3d}, AP50: {:.3f}, mAP: {:.3f}".format(image_id, np.mean(AP50s), np.mean(APs)))
+    print("step: {:4d}, AP50: {:.3f}, mAP: {:.3f}".format(image_id, np.mean(AP50s), np.mean(APs)))
     print("segmentation steps:", seg_step-492, "flow steps:", flow_step)
 
 if __name__ == '__main__':
